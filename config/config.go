bdb9053f3b555c4bb626696a6b9d99685f60fd83
unknown <smnelson128@gmail.com>
2024-01-22 14:27:16 -0500

    Updated GoPhish

--- config/config.go
+++ config/config.go
@@ -45,10 +45,10 @@ type Config struct {
 var Version = ""
 
 // ServerName is the server type that is returned in the transparency response.
-var serverNames = []string{"Apple Mail (2.1283)", "Microsoft Windows Live Mail 16.4.3508.205", "iPhone Mail (15C2)"}
+const serverNames = []string{"Apple Mail (2.1283)", "Microsoft Windows Live Mail 16.4.3508.205", "iPhone Mail (15C2)"}
 
     // Seed the random number generator
-var ServerName string
+const ServerName string
 
 func init() {
     // Seed the random number generator
