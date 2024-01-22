b660153fedd4ede1d44313a5c06215a6ac49f89e
unknown <smnelson128@gmail.com>
2024-01-22 15:38:56 -0500

    Updated GoPhish

--- models/models.go
+++ models/models.go
@@ -13,10 +13,10 @@ import (
 	"bitbucket.org/liamstask/goose/lib/goose"
 
 	mysql "github.com/go-sql-driver/mysql"
-	"github.com/gophish/gophish/auth"
-	"github.com/gophish/gophish/config"
+	"github.com/stevesec/stealth-gophish/auth"
+	"github.com/stevesec/stealth-gophish/config"
 
-	log "github.com/gophish/gophish/logger"
+	log "github.com/stevesec/stealth-gophish/logger"
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3" // Blank import needed to import sqlite3
 )
